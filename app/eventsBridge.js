1e80139769478aae41ee43530e07f053f5fa736a
Jonathan <brafdlog@gmail.com>
2019-05-04 14:48:34 +0300

    Remove debugger and make main point to prod

--- app/eventsBridge.js
+++ app/eventsBridge.js
@@ -3,12 +3,10 @@ const events = require('./constants/events');
 const runScraper = require('./node/index');
 
 ipcMain.on(events.scraping.start, async event => {
-  debugger;
   try {
     const response = await runScraper();
     event.sender.send(events.scraping.done, response);
   } catch (e) {
     event.sender.send(events.scraping.error, e);
   }
-
 });
