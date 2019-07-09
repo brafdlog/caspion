f08be8dc0445a74a0c63acf86aaf78004fdc98db
Jonathan <brafdlog@gmail.com>
2019-07-09 20:50:57 +0300

    Remove App and Routes

--- app/web/components/Root.jsx
+++ app/web/components/Root.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Routes from './Routes';
+import { HashRouter, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Config from './Config';
+import styles from './Root.css';
 
 export default class Root extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+      <React.StrictMode>
+        <div className={styles.appWrapper}>
+          <HashRouter>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/config" component={Config} />
+          </HashRouter>
+        </div>
+      </React.StrictMode>
     );
   }
 }
