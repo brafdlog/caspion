a890364fc3acda03cc6b463b0dadd30bccf657cc
Jonathan <brafdlog@gmail.com>
2019-07-09 21:34:03 +0300

    Make config a functional component using hooks

--- app/web/components/pages/config/Config.jsx
+++ app/web/components/pages/config/Config.jsx
@@ -1,47 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button } from '@material-ui/core';
 import { ipcRenderer } from 'electron';
 import styles from './Config.css';
 import events from '../../../../constants/events';
 import { jsonStringifyPretty } from '../../../webUtils';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.saveConfig = this.saveConfig.bind(this);
+const Config = () => {
+  const [configStr, setConfigStr] = useState('{}');
+
+  function saveConfig() {
+    ipcRenderer.send(events.config.updateConfig, JSON.parse(configStr));
   }
 
-  componentDidMount() {
+  useEffect(() => {
     ipcRenderer.send(events.config.getCurrentConfig);
-    ipcRenderer.on(events.config.gotCurrentConfig, (event, config) =>
-      this.setState({
-        config: jsonStringifyPretty(config)
-      })
-    );
-  }
+    ipcRenderer.on(events.config.gotCurrentConfig, (event, updatedConfig) => {
+      setConfigStr(jsonStringifyPretty(updatedConfig));
+    });
+  }, []);
 
-  saveConfig() {
-    const { config } = this.state;
-    ipcRenderer.send(events.config.updateConfig, JSON.parse(config));
-  }
+  return (
+    <div className={styles.container}>
+      <h1>config</h1>
+      <TextField className={styles.configTextField} value={configStr} multiline rows={10} onChange={event => setConfigStr(event.target.value)} />
+      <Button variant="contained" color="primary" onClick={saveConfig}>
+        Save
+      </Button>
+    </div>
+  );
+};
 
-  render() {
-    const { config } = this.state;
-    return (
-      <div className={styles.container}>
-        <h1>config</h1>
-        <TextField
-          className={styles.configTextField}
-          value={config}
-          multiline
-          rows={10}
-          onChange={event => this.setState({ config: event.target.value })}
-        />
-        <Button variant="contained" color="primary" onClick={this.saveConfig}>
-          Save
-        </Button>
-      </div>
-    );
-  }
-}
+export default Config;
