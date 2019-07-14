a22ab7931c49bec89cd7b5069d59e7457256f141
Jonathan <brafdlog@gmail.com>
2019-07-14 18:10:38 +0300

    Extract the logic of fetching and saving the config out of the config component

--- app/web/components/Root.jsx
+++ app/web/components/Root.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { HashRouter, Route } from 'react-router-dom';
+import { ipcRenderer } from 'electron';
 import HomePage from './pages/HomePage';
 import Config from './pages/config/Config';
 import Sidebar from './sidebar/Sidebar';
 import styles from './Root.css';
+import events from '../../constants/events';
 
-const Root = () => (
-  <div className={styles.appWrapper}>
-    <HashRouter>
-      <Sidebar />
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/config" component={Config} />
-    </HashRouter>
-  </div>
-);
+const Root = () => {
+  const [config, setConfig] = useState({});
+
+  function saveConfig(updatedConfig) {
+    setConfig(updatedConfig);
+    ipcRenderer.send(events.config.updateConfig, updatedConfig);
+  }
+
+  useEffect(() => {
+    ipcRenderer.send(events.config.getCurrentConfig);
+    ipcRenderer.on(events.config.gotCurrentConfig, (event, updatedConfig) => {
+      setConfig(updatedConfig);
+    });
+  }, []);
+
+  return (
+    <div className={styles.appWrapper}>
+      <HashRouter>
+        <Sidebar />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/config" render={props => <Config {...props} config={config} saveConfig={saveConfig} />} />
+      </HashRouter>
+    </div>
+  );
+};
 
 export default Root;
--- app/web/components/pages/config/Config.jsx
+++ app/web/components/pages/config/Config.jsx
@@ -1,29 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { TextField, Button } from '@material-ui/core';
-import { ipcRenderer } from 'electron';
 import styles from './Config.css';
-import events from '../../../../constants/events';
 import { jsonStringifyPretty } from '../../../webUtils';
 
-const Config = () => {
-  const [configStr, setConfigStr] = useState('{}');
-
-  function saveConfig() {
-    ipcRenderer.send(events.config.updateConfig, JSON.parse(configStr));
-  }
+const Config = ({ config, saveConfig }) => {
+  const [configStr, setConfigStr] = useState(jsonStringifyPretty(config));
 
   useEffect(() => {
-    ipcRenderer.send(events.config.getCurrentConfig);
-    ipcRenderer.on(events.config.gotCurrentConfig, (event, updatedConfig) => {
-      setConfigStr(jsonStringifyPretty(updatedConfig));
-    });
-  }, []);
+    setConfigStr(jsonStringifyPretty(config));
+  }, [config]);
 
   return (
     <div className={styles.container}>
-      <h1>config</h1>
+      <h1>הגדרות</h1>
       <TextField className={styles.configTextField} value={configStr} multiline rows={10} onChange={event => setConfigStr(event.target.value)} />
-      <Button variant="contained" color="primary" onClick={saveConfig}>
+      <Button variant="contained" color="primary" onClick={() => saveConfig(JSON.parse(configStr))}>
         Save
       </Button>
     </div>
