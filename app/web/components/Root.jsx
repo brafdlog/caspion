1c8513c787866a5bcdc47cef4f324ee05445e0c9
Jonathan <brafdlog@gmail.com>
2019-07-14 17:42:26 +0300

    Make root a functional component

--- app/web/components/Root.jsx
+++ app/web/components/Root.jsx
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Config from './pages/config/Config';
 import Sidebar from './sidebar/Sidebar';
 import styles from './Root.css';
 
-export default class Root extends Component {
-  render() {
-    return (
-      <div className={styles.appWrapper}>
-        <HashRouter>
-          <Sidebar />
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/config" component={Config} />
-        </HashRouter>
-      </div>
-    );
-  }
-}
+const Root = () => (
+  <div className={styles.appWrapper}>
+    <HashRouter>
+      <Sidebar />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/config" component={Config} />
+    </HashRouter>
+  </div>
+);
+
+export default Root;
