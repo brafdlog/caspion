fb665b89d7dd3bf7c08b364d4eb11046ded020de
Jonathan <brafdlog@gmail.com>
2019-10-08 15:47:18 +0300

    Extract financial account configuration to a file

--- app/web/components/pages/config/FinancialAccountConfig.js
+++ app/web/components/pages/config/FinancialAccountConfig.js
@@ -3,31 +3,14 @@ import React, { useState, useEffect } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import { Edit, Save } from '@material-ui/icons';
 import styles from './FinancialAccountConfig.scss';
-
-// 'hapoalim', 'leumi', 'discount', 'otsarHahayal', 'visaCal', 'leumiCard', 'isracard', 'amex'
-const COMPANY_ID_TO_METADATA = {
-  leumi: {
-    logoImageSrc:
-      'https://is5-ssl.mzstatic.com/image/thumb/Purple113/v4/79/50/3b/79503b03-e956-60a4-f9e8-aaeffc388b06/AppIcon-0-1x_U007emarketing-0-0-85-220-0-4.png/246x0w.jpg',
-    name: 'לאומי'
-  },
-  visaCal: {
-    logoImageSrc: 'https://cdn.freebiesupply.com/logos/large/2x/visa-2-logo-png-transparent.png',
-    name: 'ויזה כאל'
-  },
-  leumiCard: {
-    logoImageSrc:
-      'https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/e6/d3/40/e6d34009-8df4-0755-7b78-236562749bc3/AppIcon-0-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-220-0-0-0-3.png/246x0w.jpg',
-    name: 'לאומי קארד - max'
-  }
-};
+import { FINANCIAL_COMPANIES_CONFIG } from '../../../constants/financialCompaniesConfig';
 
 const FinancialAccountConfig = props => {
   const [editing, setEditing] = useState(false);
   const [companyId, setCompanyId] = useState(props.companyId);
   const [username, setUsername] = useState(props.credentials.username);
   const [password, setPassword] = useState(props.credentials.password);
-  const companyMetadata = COMPANY_ID_TO_METADATA[companyId];
+  const companyMetadata = FINANCIAL_COMPANIES_CONFIG[companyId];
 
   useEffect(() => {
     setCompanyId(props.companyId);
@@ -43,7 +26,7 @@ const FinancialAccountConfig = props => {
   return (
     <div className={styles.wrapper}>
       <Avatar className={styles.logo} src={companyMetadata.logoImageSrc} />
-      <span className={styles.companyName}>{companyMetadata.name}</span>
+      <span className={styles.companyName}>{companyMetadata.displayName}</span>
       {editing ? (
         <>
           <input value={username} onChange={e => setUsername(e.target.value)} />
---
+++ app/web/constants/financialCompaniesConfig.js
@@ -0,0 +1,51 @@
+export const FINANCIAL_COMPANIES = {
+  LEUMI: 'leumi',
+  VISA_CAL: 'visaCal',
+  LEUMI_CARD: 'leumiCard',
+  HAPOALIM: 'hapoalim',
+  DISCOUNT: 'discount',
+  OTSAR_HAHAYAL: 'otsarHahayal',
+  ISRACARD: 'isracard',
+  AMERICAN_EXPRESS: 'amex'
+};
+
+export const FINANCIAL_COMPANIES_CONFIG = {
+  [FINANCIAL_COMPANIES.LEUMI]: {
+    logoImageSrc:
+      'https://is5-ssl.mzstatic.com/image/thumb/Purple113/v4/79/50/3b/79503b03-e956-60a4-f9e8-aaeffc388b06/AppIcon-0-1x_U007emarketing-0-0-85-220-0-4.png/246x0w.jpg',
+    displayName: 'לאומי'
+  },
+  [FINANCIAL_COMPANIES.VISA_CAL]: {
+    logoImageSrc: 'https://cdn.freebiesupply.com/logos/large/2x/visa-2-logo-png-transparent.png',
+    displayName: 'ויזה כאל'
+  },
+  [FINANCIAL_COMPANIES.LEUMI_CARD]: {
+    logoImageSrc:
+      'https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/e6/d3/40/e6d34009-8df4-0755-7b78-236562749bc3/AppIcon-0-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-220-0-0-0-3.png/246x0w.jpg',
+    displayName: 'לאומי קארד - max'
+  },
+  [FINANCIAL_COMPANIES.HAPOALIM]: {
+    logoImageSrc: 'https://yt3.ggpht.com/a/AGF-l7-v56BwWUBUyi-1YaqthhcMvj1-ks-SFYGIgg=s288-c-k-c0xffffffff-no-rj-mo',
+    displayName: 'הפועלים'
+  },
+  [FINANCIAL_COMPANIES.DISCOUNT]: {
+    logoImageSrc: 'https://yt3.ggpht.com/a/AGF-l78kCMr0vvCd49lr6FzD4FN2kM9BF2NbI9hI0A=s900-c-k-c0xffffffff-no-rj-mo',
+    displayName: 'דיסקונט'
+  },
+  [FINANCIAL_COMPANIES.OTSAR_HAHAYAL]: {
+    logoImageSrc: 'https://is2-ssl.mzstatic.com/image/thumb/Purple113/v4/65/d4/93/65d49347-7d3a-07ef-c468-cb0122db7f07/source/256x256bb.jpg',
+    displayName: 'אוצר החייל'
+  },
+  [FINANCIAL_COMPANIES.ISRACARD]: {
+    logoImageSrc: 'https://static-s.aa-cdn.net/img/ios/489783331/01220130f45da0bd6c11eb81cb1c0862?v=1',
+    displayName: 'ישראכרט'
+  },
+  [FINANCIAL_COMPANIES.AMERICAN_EXPRESS]: {
+    logoImageSrc: 'https://d27dymkl0d7xsb.cloudfront.net/images/verifications/logos/american-express-logo-6fcf393b.png',
+    displayName: 'אמריקן אקספרס'
+  }
+};
+
+Object.keys(FINANCIAL_COMPANIES_CONFIG).forEach(companyId => {
+  FINANCIAL_COMPANIES_CONFIG[companyId].id = companyId;
+});
