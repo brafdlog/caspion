bdb500a67222c435833bacf09b1a61cae5526592
Baruch Rothkoff <baruchiro@gmail.com>
2019-10-08 10:41:24 +0300

    Initial Vue commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <landing-page></landing-page>
+  </div>
+</template>
+
+<script>
+  import LandingPage from '@/components/LandingPage'
+
+  export default {
+    name: 'israeli-bank-scrapers-desktop',
+    components: {
+      LandingPage
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
