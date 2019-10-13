68f3c8bb5ec9179efcd6072d3f885f0d866133a7
Baruch Rothkoff <baruchiro@gmail.com>
2019-10-13 14:00:24 +0300

    Remove original content

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -7,23 +7,6 @@
         </span>
         <Importers></Importers>
       </div>
-
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
     </main>
   </div>
 </template>
--- src/renderer/components/MainPage/Importers.vue
+++ src/renderer/components/MainPage/Importers.vue
@@ -11,24 +11,6 @@
       :key="scraper.name" 
       :scraper="scraper" />
     </div>
-    <div class="items">
-      <div class="item">
-        <div class="name">Vue.js:</div>
-        <div class="value">{{ vue }}</div>
-      </div>
-      <div class="item">
-        <div class="name">Electron:</div>
-        <div class="value">{{ electron }}</div>
-      </div>
-      <div class="item">
-        <div class="name">Node:</div>
-        <div class="value">{{ node }}</div>
-      </div>
-      <div class="item">
-        <div class="name">Platform:</div>
-        <div class="value">{{ platform }}</div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -39,11 +21,7 @@
   export default {
     data () {
       return {
-        isShowOptionalImporters: false,
-        electron: process.versions.electron,
-        node: process.versions.node,
-        platform: require('os').platform(),
-        vue: require('vue/package.json').version
+        isShowOptionalImporters: false
       }
     },
     computed: {
@@ -51,7 +29,7 @@
         scrapers: state => state.Scrapers.scrapers
       })
     },
-    components: {AddScraper}
+    components: { AddScraper }
   }
 </script>
 
