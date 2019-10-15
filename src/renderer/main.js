e588396ce000be40694cb1a84df27a4e7700d7e7
Baruch Rothkoff <baruchiro@gmail.com>
2019-10-15 11:47:53 +0300

    Add babel plugin

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 
+import { Button, Select } from 'element-ui'
+
 import App from './App'
 import store from './store'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
+
+Vue.use(Button)
+Vue.use(Select)
 
 /* eslint-disable no-new */
 new Vue({
