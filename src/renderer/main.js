4fc5fbae61addf1cdda3172b17ba53aab56a85eb
Baruch Rothkoff <baruchiro@gmail.com>
2019-10-15 11:50:36 +0300

    Move to English

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 
 import { Button, Select } from 'element-ui'
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
 
 import App from './App'
 import store from './store'
@@ -8,6 +10,8 @@ import store from './store'
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
+// configure language
+locale.use(lang)
 Vue.use(Button)
 Vue.use(Select)
 
