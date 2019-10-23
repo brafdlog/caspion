6486bf55b055a2af87cd2fe8e23b59eaefe68a3b
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-10-23 18:14:04 +0300

    Download Chromium

---
+++ src/renderer/modules/downloadChromium.js
@@ -0,0 +1,8 @@
+import download from 'download-chromium'
+import { getPuppeteerConfig } from 'israeli-bank-scrapers-core'
+
+const chromiumVersion = getPuppeteerConfig().chromiumRevision
+
+export default async function () {
+  return download({revision: chromiumVersion})
+}
