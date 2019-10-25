22e517f0f217abac7ab043df74c5ef267856d4c7
Baruch Rothkoff <baruchiro@gmail.com>
2019-10-25 08:47:37 +0300

    Move to airbnb eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     browser: true,
     node: true
   },
-  extends: 'standard',
+  extends: 'airbnb-base',
   globals: {
     __static: true
   },
@@ -16,10 +16,13 @@ module.exports = {
     'html'
   ],
   'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
+    'global-require': 0,
+    'import/no-unresolved': 0,
+    'no-param-reassign': 0,
+    'no-shadow': 0,
+    'import/extensions': 0,
+    'import/newline-after-import': 0,
+    'no-multi-assign': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
