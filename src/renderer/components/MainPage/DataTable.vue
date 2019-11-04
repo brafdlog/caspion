d9a3964be034787c2c7be062baea0f5eff0b7022
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-11-04 07:42:52 +0200

    lint:fix

--- src/renderer/components/MainPage/DataTable.vue
+++ src/renderer/components/MainPage/DataTable.vue
@@ -1,24 +1,28 @@
 <template>
-<el-table
-  :data="transactions"
-  :default-sort="{prop: 'date', order: 'descending'}"
-  border
-  stripe>
-  <el-table-column type="expand">
-    <template slot-scope="props">
-      <p v-for="(value, prop) in propertiesColumns" :key="prop">
-        {{ value }}: {{ props.row[prop] }}
-      </p>
-    </template>
-  </el-table-column>
-  <el-table-column
-    v-for="(value, prop) in tableColumns"
-    :key="prop"
-    :prop="prop"
-    :label="value"
-    sortable>
-  </el-table-column>
-</el-table>
+  <el-table
+    :data="transactions"
+    :default-sort="{prop: 'date', order: 'descending'}"
+    border
+    stripe
+  >
+    <el-table-column type="expand">
+      <template slot-scope="props">
+        <p
+          v-for="(value, prop) in propertiesColumns"
+          :key="prop"
+        >
+          {{ value }}: {{ props.row[prop] }}
+        </p>
+      </template>
+    </el-table-column>
+    <el-table-column
+      v-for="(value, prop) in tableColumns"
+      :key="prop"
+      :prop="prop"
+      :label="value"
+      sortable
+    />
+  </el-table>
 </template>
 
 <script>
