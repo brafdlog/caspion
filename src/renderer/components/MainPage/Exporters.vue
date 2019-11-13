864cfa4be1c165ee45f29bb9aa370df537d88d81
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-11-13 20:57:59 +0200

    Add first JsonExporter

--- src/renderer/components/MainPage/Exporters.vue
+++ src/renderer/components/MainPage/Exporters.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>Hi</div>
+  <el-collapse v-model="accordionActiveItem" accordion>
+  <el-collapse-item title="Export to Json" name="1">
+    <json-exporter />
+  </el-collapse-item>
+</el-collapse>
 </template>
 
 <script>
+import JsonExporter from './Exporters/JsonExporter';
+
 export default {
+  components: { JsonExporter },
+  data() {
+    return {
+      accordionActiveItem: '',
+    };
+  },
 };
 </script>
 
---
+++ src/renderer/components/MainPage/Exporters/JsonExporter.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-form>
+    <el-form-item label="Folder to save">
+      <el-input v-model="folder"/>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  name: 'JsonExporter',
+  data() {
+    return {
+      folder: '',
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
