2df3b9d88ce34c4024fae77df529ff2eb29f17b9
ariel <arielgordon123@gmail.com>
2020-01-16 09:28:54 +0200

    Test app title

--- test/e2e/specs/Launch.spec.js
+++ test/e2e/specs/Launch.spec.js
@@ -1,11 +1,29 @@
-import utils from '../utils';
+const { testWithSpectron } = require('vue-cli-plugin-electron-builder');
 
-describe('Launch', () => {
-  beforeEach(utils.beforeEach);
-  afterEach(utils.afterEach);
+jest.setTimeout(100000);
 
-  it('shows the proper application title', () => this.app.client.getTitle()
-    .then((title) => {
-      expect(title).to.equal('israeli-bank-scrapers-desktop');
-    }));
+test('shows the proper application title', async () => {
+  // Wait for dev server to start
+  const { app, stopServe } = await testWithSpectron();
+  const win = app.browserWindow;
+  const { client } = app;
+
+  // Window was created
+  expect(await client.getWindowCount()).toBe(1);
+  // It is not minimized
+  expect(await win.isMinimized()).toBe(false);
+  // Window is visible
+  expect(await win.isVisible()).toBe(true);
+  // Size is correct
+  const { width, height } = await win.getBounds();
+  expect(width).toBeGreaterThan(0);
+  expect(height).toBeGreaterThan(0);
+  // App is loaded properly
+  expect(
+    /israeli-bank-scrapers-desktop/.test(
+      await client.getHTML('#app'),
+    ),
+  ).toBe(true);
+
+  await stopServe();
 });
