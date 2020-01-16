1d54848722e7fd6373d8c080703eaebff89c4497
Baruch Rothkoff <baruchiro@gmail.com>
2020-01-16 13:35:20 +0200

    Before and After

--- test/e2e/specs/Launch.spec.js
+++ test/e2e/specs/Launch.spec.js
@@ -2,28 +2,38 @@ const { testWithSpectron } = require('vue-cli-plugin-electron-builder');
 
 jest.setTimeout(100000);
 
-test('shows the proper application title', async () => {
-  // Wait for dev server to start
-  const { app, stopServe } = await testWithSpectron();
-  const win = app.browserWindow;
-  const { client } = app;
+describe('Launch', () => {
+  let app;
+  let stopServe;
 
-  // Window was created
-  expect(await client.getWindowCount()).toBe(1);
-  // It is not minimized
-  expect(await win.isMinimized()).toBe(false);
-  // Window is visible
-  expect(await win.isVisible()).toBe(true);
-  // Size is correct
-  const { width, height } = await win.getBounds();
-  expect(width).toBeGreaterThan(0);
-  expect(height).toBeGreaterThan(0);
-  // App is loaded properly
-  expect(
-    /israeli-bank-scrapers-desktop/.test(
-      await client.getHTML('#app'),
-    ),
-  ).toBe(true);
+  beforeEach(async () => {
+    ({ app, stopServe } = await testWithSpectron());
+  });
 
-  await stopServe();
+  test('shows the proper application title', async () => {
+    // Wait for dev server to start
+    const win = app.browserWindow;
+    const { client } = app;
+
+    // Window was created
+    expect(await client.getWindowCount()).toBe(1);
+    // It is not minimized
+    expect(await win.isMinimized()).toBe(false);
+    // Window is visible
+    expect(await win.isVisible()).toBe(true);
+    // Size is correct
+    const { width, height } = await win.getBounds();
+    expect(width).toBeGreaterThan(0);
+    expect(height).toBeGreaterThan(0);
+    // App is loaded properly
+    expect(
+      /israeli-bank-scrapers-desktop/.test(
+        await client.getHTML('#app'),
+      ),
+    ).toBe(true);
+  });
+
+  afterEach(async () => {
+    await stopServe();
+  });
 });
