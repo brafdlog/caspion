ea41571e5ea4e8354c719c04513283c6c3feae44
Baruch Rothkoff <baruchiro@gmail.com>
2020-01-16 16:03:01 +0200

    Revert "Format"
    This reverts commit ce786e9344f05b8848279fa5e1aade0edd4b212b.

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,19 @@
-import { defaults } from 'jest-config';
+const { defaults } = require('jest-config');
 
-export default {
+module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-  testMatch:
-    [
-      '**/__test__/**/*.[jt]s?(x)',
-      '**/?(*.)+(spec|test).[jt]s?(x)',
-    ],
-  roots: [
-    'test/unit',
-    'src',
-  ],
-  moduleFileExtensions: [
-    ...defaults.moduleFileExtensions,
-    'vue',
-  ],
-  moduleDirectories: [
-    ...defaults.moduleDirectories,
-    'src',
-  ],
+  testMatch: ['**/__test__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  roots: ['test/unit', 'src'],
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'vue'],
+  moduleDirectories: [...defaults.moduleDirectories, 'src'],
   transformIgnorePatterns: [
-    'node_modules/(?!(babel-jest|jest-vue-preprocessor)/)',
+    "node_modules/(?!(babel-jest|jest-vue-preprocessor)/)"
   ],
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy',
-    electron: '<rootDir>/test/unit/mock/electron.js',
+    'electron': '<rootDir>/test/unit/mock/electron.js'
   },
-  transform:
-  {
+  transform: {
     '.*\\.(vue)$': 'vue-jest',
     '^.+\\.js$': 'babel-jest',
   },
