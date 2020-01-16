cf3eb2495847d507fb4458ad75d457d9e1cee979
Baruch Rothkoff <baruchiro@gmail.com>
2020-01-16 21:53:36 +0200

    Reorder

--- src/store/modules/Scrapers.js
+++ src/store/modules/Scrapers.js
@@ -2,23 +2,26 @@ const state = {
   scrapers: [],
 };
 
-export const getters = {
-  scrapersWithId: (state) => Object.keys(state.scrapers)
-    .map((k) => ({ key: k, ...state.scrapers[k] })),
-};
-const actions = {
-  set_scraper({ commit }, scrapers) {
-    commit('setScraper', scrapers);
-  },
-};
 const mutations = {
   setScraper(state, scrapers) {
     state.scrapers = scrapers;
   },
 };
+
+export const getters = {
+  scrapersWithId: (state) => Object.keys(state.scrapers)
+    .map((k) => ({ key: k, ...state.scrapers[k] })),
+};
+
+const actions = {
+  set_scraper({ commit }, scrapers) {
+    commit('setScraper', scrapers);
+  },
+};
+
 export default {
   state,
+  mutations,
   getters,
   actions,
-  mutations,
 };
