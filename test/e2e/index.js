2cabc5b03e407266c208aea9049f7a415c17587e
Baruch Rothkoff <baruchiro@gmail.com>
2020-01-16 14:03:39 +0200

    Unused packages

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -9,12 +9,5 @@ require('@babel/register')({
   ignore: [/node_modules/],
 });
 
-// Attach Chai APIs to global scope
-const { expect, should, assert } = require('chai');
-
-global.expect = expect;
-global.should = should;
-global.assert = assert;
-
 // Require all JS files in `./specs` for Mocha to consume
 require('require-dir')('./specs');
