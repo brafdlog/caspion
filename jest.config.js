ce786e9344f05b8848279fa5e1aade0edd4b212b
Baruch Rothkoff <baruchiro@gmail.com>
2020-01-16 15:59:01 +0200

    Format

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,33 @@
-const { defaults } = require('jest-config');
+import { defaults } from 'jest-config';
 
-module.exports = {
+export default {
   preset: '@vue/cli-plugin-unit-jest',
-  testMatch: ['**/__test__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-  roots: ['test/unit', 'src'],
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'vue'],
-  moduleDirectories: [...defaults.moduleDirectories, 'src'],
+  testMatch:
+    [
+      '**/__test__/**/*.[jt]s?(x)',
+      '**/?(*.)+(spec|test).[jt]s?(x)',
+    ],
+  roots: [
+    'test/unit',
+    'src',
+  ],
+  moduleFileExtensions: [
+    ...defaults.moduleFileExtensions,
+    'vue',
+  ],
+  moduleDirectories: [
+    ...defaults.moduleDirectories,
+    'src',
+  ],
   transformIgnorePatterns: [
-    "node_modules/(?!(babel-jest|jest-vue-preprocessor)/)"
+    'node_modules/(?!(babel-jest|jest-vue-preprocessor)/)',
   ],
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy',
-    'electron': '<rootDir>/test/unit/mock/electron.js'
+    electron: '<rootDir>/test/unit/mock/electron.js',
   },
-  transform: {
+  transform:
+  {
     '.*\\.(vue)$': 'vue-jest',
     '^.+\\.js$': 'babel-jest',
   },
