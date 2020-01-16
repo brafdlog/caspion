9f6db3a47d4cd8bc09e6c8d06f97e3eadc6c3e49
Baruch Rothkoff <baruchiro@gmail.com>
2020-01-16 20:41:45 +0200

    electron-builder config

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,44 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       // List native deps here if they don't work
-      externals: ["keytar"]
+      externals: ['keytar'],
+      builderOptions: {
+        productName: 'israeli-bank-scrapers-desktop',
+        appId: 'com..electron.israeli-bank-scrapers-desktop',
+        files: [
+          'dist/electron/**/*',
+        ],
+        dmg: {
+          contents: [
+            {
+              x: 410,
+              y: 150,
+              type: 'link',
+              path: '/Applications',
+            },
+            {
+              x: 130,
+              y: 150,
+              type: 'file',
+            },
+          ],
+        },
+        mac: {
+          icon: 'build/icons/icon.icns',
+        },
+        win: {
+          icon: 'build/icons/icon.ico',
+        },
+        linux: {
+          target: [
+            'AppImage',
+            'snap',
+            'deb',
+          ],
+          icon: 'build/icons',
+        },
+      },
     },
-    mainProcessWatch: ["src/service"]
-  }
+    mainProcessWatch: ['src/service'],
+  },
 };
