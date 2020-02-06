395f340800963356c2bdf0666629524d35838980
Baruch Rothkoff <baruchiro@gmail.com>
2020-02-06 17:41:51 +0200

    Implement the method

--- test/e2e/utils/interactions.js
+++ test/e2e/utils/interactions.js
@@ -1,9 +1,17 @@
+const CollapseAddImporter = 'div[data-test="CollapseAddImporter"]';
+const AddScrapers = `${CollapseAddImporter} div[data-test]`;
+
 export default class Interactions {
+
   constructor(client) {
     this.client = client;
   }
 
   async clickCollapseAddImporter() {
-    return this.client.findElement('[data-test="CollapseAddImporter"]').click();
+    return this.client.findElement(CollapseAddImporter).click();
+  }
+
+  async getAddScrapers() {
+    return this.client.findElements(AddScrapers);
   }
 }
