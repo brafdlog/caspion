2b7929dbca054b9d642c8a2ab90b6088c5b30810
Baruch Rothkoff <baruchiro@gmail.com>
2020-02-16 16:54:31 +0200

    Log where to save screenshot

--- test/e2e/specs/Launch.spec.js
+++ test/e2e/specs/Launch.spec.js
@@ -71,6 +71,8 @@ const skip = process.env.GITHUB_ACTIONS && process.platform === 'win32';
 
     if (global.lastTest.failed) {
       const screenshotFile = path.join(screenshotsDir, `${global.lastTest.test.name.trim()}.png`);
+      // eslint-disable-next-line no-console
+      console.log(`Test '${global.lastTest.test.name}' failed. Screenshot will be saved on '${screenshotFile}'`);
       const imgBuffer = await win.capturePage();
       fs.writeFileSync(screenshotFile, imgBuffer);
     }
