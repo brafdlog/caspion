9142fb9a02a01e7c8e5a39c13c99a827a3970bd4
Baruch Rothkoff <baruchiro@gmail.com>
2020-04-10 14:42:12 +0300

    Get latest files

--- nuxt/components/HeroContent.vue
+++ nuxt/components/HeroContent.vue
@@ -17,12 +17,40 @@
           בצורה מאובטחת, ללא צד שלישי ובפיקוח הקהילה
         </p>
 
-        <button
-          class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg"
-          @click.native="download"
-        >
-          התקן עכשיו
-        </button>
+        <div class="mx-auto my-6">
+          <button
+            class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full py-4 px-8 shadow-lg"
+            @click="download"
+          >
+            התקן עכשיו
+          </button>
+          <div class="mx-auto text-center">
+            <button @click="moreDownloads">
+              הורדות נוספות
+            </button>
+            <div
+              class="w-full text-right"
+              :style="{visibility: showDownloads? 'visible' : 'hidden'}"
+            >
+              <ul>
+                <li
+                  v-for="obj in downloads"
+                  :key="obj.name"
+                >
+                  <a
+                    class="no-underline hover:underline text-blue-600"
+                    :href="obj.url"
+                    :title="obj.name"
+                  >קובץ {{ obj.extension }}</a>
+                </li>
+              </ul>
+              <a
+                class="text-sm text-gray-800 font-bold"
+                href="https://github.com/baruchiro/israeli-bank-scrapers-desktop/releases"
+              >עוד...</a>
+            </div>
+          </div>
+        </div>
       </div>
       <!--Right Col-->
       <div class="w-full md:w-3/5 py-6 text-center">
@@ -37,23 +65,98 @@
 </template>
 
 <script>
+import axios from 'axios';
+
+const getOS = () => {
+  if (process.client) {
+    // This script sets OSName variable as follows:
+    // "Windows"    for all versions of Windows
+    // "MacOS"      for all versions of Macintosh OS
+    // "Linux"      for all versions of Linux
+    // "UNIX"       for all other UNIX flavors
+    // "Unknown" indicates failure to detect the OS
+
+    if (navigator.appVersion.indexOf('Win') !== -1) return 'Windows';
+    if (navigator.appVersion.indexOf('Mac') !== -1) return 'MacOS';
+    if (navigator.appVersion.indexOf('Linux') !== -1) return 'Linux';
+    if (navigator.appVersion.indexOf('X11') !== -1) return 'UNIX';
+  }
+
+  return 'Unknown';
+};
+
+const osToExtension = {
+  Linux: {
+    default: 'AppImage',
+    extensions: ['AppImage', 'deb'],
+  },
+  UNIX: {
+    default: 'AppImage',
+    extensions: ['AppImage', 'deb'],
+  },
+  Windows: {
+    default: 'exe',
+    extensions: ['exe'],
+  },
+};
+
+const excludeExtensions = ['blockmap', 'yml'];
+
+const isWithExtension = (extensions, name) => extensions.filter((ext) => name.toLowerCase().endsWith(ext.toLowerCase())).length > 0;
+
 export default {
   name: 'HeroContent',
+  data() {
+    return {
+      downloads: [],
+      os: 'Unknown',
+      showDownloads: false,
+    };
+  },
+  created() {
+    this.os = getOS();
+
+    axios.get('https://api.github.com/repos/baruchiro/israeli-bank-scrapers-desktop/releases')
+      .then((response) => response.data)
+      .then((releases) => releases
+        .sort((a, b) => new Date(b.published_at) - new Date(a.published_at))
+        .find((release) => !release.draft))
+      .then((release) => {
+        const osExtension = osToExtension[this.os];
+
+        const filteredAssets = osExtension && osExtension.extensions.length > 0
+          ? release.assets.filter((asset) => isWithExtension(osExtension.extensions, asset.name))
+          : release.assets;
+
+        this.downloads = filteredAssets
+          .filter((asset) => !isWithExtension(excludeExtensions, asset.name))
+          .map(({ name, browser_download_url: url }) => {
+            const splitted = name.split('.');
+            const extension = splitted[splitted.length - 1];
+            return {
+              name,
+              url,
+              extension,
+            };
+          });
+      });
+  },
   methods: {
     download() {
-      // This script sets OSName variable as follows:
-      // "Windows"    for all versions of Windows
-      // "MacOS"      for all versions of Macintosh OS
-      // "Linux"      for all versions of Linux
-      // "UNIX"       for all other UNIX flavors
-      // "Unknown OS" indicates failure to detect the OS
-
-      let OSName = 'Unknown OS';
-      if (navigator.appVersion.indexOf('Win') !== -1) OSName = 'Windows';
-      if (navigator.appVersion.indexOf('Mac') !== -1) OSName = 'MacOS';
-      if (navigator.appVersion.indexOf('X11') !== -1) OSName = 'UNIX';
-      if (navigator.appVersion.indexOf('Linux') !== -1) OSName = 'Linux';
-      window.open(`https://www.google.com?q=${OSName}`, '_blank');
+      const defaultExt = osToExtension[this.os].default;
+      const download = this.downloads.find(({ name }) => name.toLowerCase().endsWith(defaultExt.toLowerCase()));
+      if (download) {
+        window.open(download.url);
+      } else {
+        window.open('https://github.com/baruchiro/israeli-bank-scrapers-desktop/releases', '_blank');
+      }
+    },
+    moreDownloads() {
+      if (this.downloads.length > 1) {
+        this.showDownloads = !this.showDownloads;
+      } else {
+        window.open('https://github.com/baruchiro/israeli-bank-scrapers-desktop/releases', '_blank');
+      }
     },
   },
 };
--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -4,14 +4,14 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'עוש"י',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content: 'ניהול הוצאות',
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
