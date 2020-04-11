e7d1f3e1e353cd76b6790a3d2f9f2f80b8a46c82
Baruch Rothkoff <baruchiro@gmail.com>
2020-04-11 21:27:37 +0300

    Contact

--- nuxt/components/CallToActionContent.vue
+++ nuxt/components/CallToActionContent.vue
@@ -1,21 +1,29 @@
 <template>
   <section class="container mx-auto text-center py-6 mb-12">
     <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-white">
-      Call to Action
+      צור קשר
     </h1>
     <div class="w-full mb-4">
       <div class="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t" />
     </div>
 
     <h3 class="my-4 text-3xl leading-tight">
-      Main Hero Message to sell yourself!
+      אנחנו שמחים לשמוע פידבקים, דיווחי באגים ובקשות לשיפור.
     </h3>
 
-    <button
-      class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg"
+    <p>
+      אם יש לך רעיון לייבוא נתונים ממקומות נוספים חוץ מאשר הבנקים וחברות האשראי.
+      או תוכנה נוספת שאפשר לייצא אליה את הנתונים.
+      נשמח מאוד לשמוע!
+    </p>
+
+    <a
+      class="inline-block mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg"
+      href="mailto:baruchiro@gmail.com"
+      target="_blank"
     >
-      Action!
-    </button>
+      שלח מייל!
+    </a>
   </section>
 </template>
 
