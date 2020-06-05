8776135ae662ff9218ec0ebf9781ecfc46fb7fc9
Jonathan <brafdlog@gmail.com>
2020-06-05 23:18:05 +0300

    Replace encryption implementation and remove hard coded key from the code

--- src/originalBudgetTrackingApp/node/configManager.js
+++ src/originalBudgetTrackingApp/node/configManager.js
@@ -1,34 +1,27 @@
-import { app } from 'electron';
 import { promisify } from 'util';
-import path from 'path';
 import fs from 'fs';
-import { encrypt, decrypt } from './encryption';
+import { encrypt, decrypt } from '../../modules/encryption/crypto';
 
 import configExample from './config-example';
 
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 
-const appDataFolderPath = app ? app.getPath('userData') : '';
 const CONFIG_FILE_NAME = 'config.json';
-const APP_DATA_CONFIG_FILE_PATH = path.join(appDataFolderPath, CONFIG_FILE_NAME);
 const LOCAL_CONFIG_FILE_PATH = CONFIG_FILE_NAME;
 
 export async function getConfig() {
   let parsedConfig;
   let configFromFile = await getConfigFromFile(LOCAL_CONFIG_FILE_PATH);
-  if (!configFromFile) {
-    // Fallback to APP_DATA config if local configuration doesn't exist.
-    configFromFile = await getConfigFromFile(APP_DATA_CONFIG_FILE_PATH);
-  }
+
   if (configFromFile) {
+    configFromFile = await decrypt(configFromFile);
     parsedConfig = JSON.parse(configFromFile);
   } else {
     // Fallback to configExample if there is no config file defined at all
     parsedConfig = configExample;
   }
-  const decryptedConfig = decryptConfigIfNeeded(parsedConfig);
-  return decryptedConfig;
+  return parsedConfig;
 }
 
 async function getConfigFromFile(configFilePath) {
@@ -49,13 +42,4 @@ export async function updateConfig(configToUpdate) {
 function encryptConfig(stringifiedConfig) {
   const encryptedConfig = encrypt(stringifiedConfig);
   return JSON.stringify(encryptedConfig);
-}
-
-function decryptConfigIfNeeded(configFromFile) {
-  const isEncrypted = configFromFile.encryptedData;
-  if (isEncrypted) {
-    const decryptedConfig = decrypt(configFromFile);
-    return JSON.parse(decryptedConfig);
-  }
-  return configFromFile;
 }
