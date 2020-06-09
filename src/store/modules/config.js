002dba02e1c102f330b69c94e7be7b852cec61d7
Baruch Rothkoff <baruchiro@gmail.com>
2020-06-09 07:50:56 +0300

    Add mutations and actions

--- src/store/modules/config.js
+++ src/store/modules/config.js
@@ -1,3 +1,5 @@
+export const ADD_IMPORTER_ACTION = 'ADD_IMPORTER_ACTION';
+
 const state = {
   scraping: {
     numDaysBack: 40,
@@ -8,6 +10,20 @@ const state = {
   monitoring: {}
 };
 
+const mutations = {
+  addImporter: (state, importer) => {
+    state.scraping.accountsToScrape.push(importer);
+  }
+};
+
+const actions = {
+  [ADD_IMPORTER_ACTION]: ({ commit }, importer) => {
+    commit('addImporter', importer);
+  }
+};
+
 export default {
-  state
+  state,
+  mutations,
+  actions,
 };
