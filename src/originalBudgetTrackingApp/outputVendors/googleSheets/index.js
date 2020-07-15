db790e10ce5e6a046f7f7ff58b2125f61b4bfc60
Baruch Rothkoff <baruchiro@gmail.com>
2020-07-15 07:49:35 +0300

    Revert "Add index.js to GoogleSheets and YNAB"
    This reverts commit 36e7e48d167418f02f66ff38da11e3baaa938a4d.

--- src/originalBudgetTrackingApp/outputVendors/googleSheets/index.js
+++
@@ -1,6 +0,0 @@
-export const displayName = 'Google Sheets';
-export const description = 'Export the transactions to Google Sheets';
-
-export const output = async (transactions, config) => {
-  console.log(transactions, config);
-};
--- src/originalBudgetTrackingApp/outputVendors/ynab/index.js
+++
@@ -1,6 +0,0 @@
-export const displayName = 'YNAB';
-export const description = 'Update your YNAB account with the transactions';
-
-export const output = async (transactions, config) => {
-  console.log(transactions, config);
-};
