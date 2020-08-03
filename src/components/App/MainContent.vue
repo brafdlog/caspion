64a8757d0878b76b869c42e10519bbc18b7311d0
Baruch Rothkoff <baruchiro@gmail.com>
2020-08-03 07:18:32 +0300

    Main button

--- src/components/App/MainContent.vue
+++ src/components/App/MainContent.vue
@@ -1,21 +1,64 @@
 <template>
-  <log-sheet
-    :value="true"
-    :raw="$logger.getLastLines(10)"
-  >
-    Run
-  </log-sheet>
+<v-container>
+  <v-row justify="center" align="center">
+          <v-btn
+        x-large
+        :loading="scraping"
+        :color="btnColor"
+        @click="scrape"
+      >
+        Run
+      </v-btn>
+  </v-row>
+  <v-row justify="conter" align="center">
+      <pre class="text-no-wrap">
+      {{ results }}
+    </pre>
+  </v-row>
+</v-container>
 </template>
 
 <script>
-import LogSheet from '@/components/shared/LogSheet';
+import { scrapeAndUpdateOutputVendors } from '@/originalBudgetTrackingApp';
+
+const colors = {
+  true: 'green',
+  false: 'red',
+  null: null
+};
 
 export default {
   name: 'MainContent',
-  components: { LogSheet }
+  data() {
+    return {
+      scraping: false,
+      results: null,
+      succeeded: null,
+    };
+  },
+  computed: {
+    btnColor() {
+      return colors[this.succeeded];
+    }
+  },
+  methods: {
+    async scrape() {
+      this.scraping = true;
+      scrapeAndUpdateOutputVendors()
+        .then((results) => {
+          this.results = results;
+        })
+        .catch((error) => {
+          this.results = error;
+        })
+        .finally(() => {
+          console.log(this.results);
+          this.scraping = false;
+        });
+    }
+  }
 };
 </script>
 
 <style>
-
 </style>
