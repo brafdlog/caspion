a766f9dae12565a9c4377abd68e5afc4a672829b
Baruch Odem <baruchiro@gmail.com>
2020-08-18 21:37:48 +0300

    never print password

--- src/originalBudgetTrackingApp/bankScraper.ts
+++ src/originalBudgetTrackingApp/bankScraper.ts
@@ -22,7 +22,7 @@ export async function scrape({
   companyId, credentials, startDate, showBrowser = false
 }: ScrapeParameters) {
   if (!credentials || (!credentials.username && !credentials.num && !credentials.id) || !credentials.password) {
-    throw new Error(`Missing credentials for scraper. CompanyId: ${companyId}. Credentials: ${credentials && JSON.stringify(credentials)}`);
+    throw new Error(`Missing credentials for scraper. CompanyId: ${companyId}`);
   }
 
   const chromePath = await getChrome(undefined, console.log);
