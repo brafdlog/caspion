bc04700b3bea3923e653a9fc2019b87c894cca93
Baruch Odem <baruchiro@gmail.com>
2020-09-08 20:46:09 +0300

    Apply direct-vuex in all components

--- src/store/modules/config.ts
+++ src/store/modules/config.ts
@@ -8,14 +8,6 @@ type GlobalConfig = {
   showBrowser: boolean
 }
 
-export const ADD_IMPORTER_ACTION = 'ADD_IMPORTER_ACTION';
-export const ADD_EXPORTER_ACTION = 'ADD_EXPORTER_ACTION';
-export const REMOVE_IMPORTER_ACTION = 'REMOVE_IMPORTER_ACTION';
-export const GET_IMPORTERS_GETTER = 'GET_IMPORTERS_GETTER';
-export const GET_EXPORTER_GETTER = 'GET_EXPORTER_GETTER';
-export const GLOBAL_CONFIG_GETTER = 'GLOBAL_CONFIG_GETTER';
-export const UPDATE_GLOBAL_CONFIG_ACTION = 'UPDATE_GLOBAL_CONFIG_ACTION';
-
 export default defineModule({
   namespaced: true as true,
   state: defaultConfig,
@@ -33,31 +25,31 @@ export default defineModule({
     }
   },
   getters: {
-    [GET_IMPORTERS_GETTER]: (state) => {
+    importersWithStatus: (state) => {
       const importers = state.scraping.accountsToScrape.map((importer) => {
         const status = {};
         return { ...importer, status };
       });
       return importers;
     },
-    [GET_EXPORTER_GETTER]: (state) => (name: string) => state.outputVendors[name],
-    [GLOBAL_CONFIG_GETTER]: ({ scraping }): GlobalConfig => {
+    getExporter: (state) => (name: string) => state.outputVendors[name],
+    globalConfig: ({ scraping }): GlobalConfig => {
       const { numDaysBack, showBrowser } = scraping;
       return { numDaysBack, showBrowser };
     }
   },
   actions: {
-    [ADD_IMPORTER_ACTION]: ({ commit }, importer) => {
+    addImporter: ({ commit }, importer) => {
       importer.id = randomHex();
       commit('addImporter', importer);
     },
-    [REMOVE_IMPORTER_ACTION]: ({ commit }, importerId) => {
+    removeImporter: ({ commit }, importerId) => {
       commit('removeImporter', importerId);
     },
-    [ADD_EXPORTER_ACTION]: ({ commit }, { name, ...values }) => {
+    addExporter: ({ commit }, { name, ...values }) => {
       commit('addExporter', { name, ...values });
     },
-    [UPDATE_GLOBAL_CONFIG_ACTION]: ({ commit }, globalConfig: GlobalConfig) => {
+    updateGlobalConfig: ({ commit }, globalConfig: GlobalConfig) => {
       commit('updateGlobalConfig', globalConfig);
     }
   },
