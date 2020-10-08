62669a8b8055d009cee8b978ca3fcfc77f529bdb
Baruch Odem <baruchiro@gmail.com>
2020-10-08 18:02:41 +0300

    Create ElectronGoogleOAuth2Connector *not compiling*

--- src/modules/spreadsheet/googleOAuth2.js
+++ src/modules/spreadsheet/googleOAuth2.js
@@ -3,7 +3,6 @@ import { saveIntoAccount, getFromAccount } from '@/modules/encryption/keytar';
 import ElectronGoogleOAuth2 from '@getstation/electron-google-oauth2';
 
 const keytarAccount = 'googleOauth2Token';
-const successRedirectURL = 'https://github.com/baruchiro/israeli-bank-scrapers-desktop';
 
 // eslint-disable-next-line camelcase
 async function saveToken({ refresh_token }) {
@@ -17,35 +16,6 @@ async function loadToken() {
   if (strToken === null) return null;
   const encryptedToken = JSON.parse(strToken);
   return decryptObject(encryptedToken);
-}
-
-export async function CreateClient() {
-  const myApiOauth = new ElectronGoogleOAuth2(
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    [
-      'https://www.googleapis.com/auth/drive.metadata.readonly',
-      'https://www.googleapis.com/auth/spreadsheets',
-    ],
-    { successRedirectURL },
-  );
-
-  myApiOauth.on('tokens', async (tokens) => {
-    if (tokens.refresh_token) {
-      await saveToken(tokens);
-    }
-  });
-
-  const savedToken = await loadToken();
-
-  if (savedToken !== null) {
-    myApiOauth.setTokens(savedToken);
-  } else {
-    const token = await myApiOauth.openAuthWindowAndGetTokens();
-    await saveToken(token);
-    myApiOauth.setTokens(token);
-  }
-  return myApiOauth.oauth2Client;
 }
 
 export async function isConnected() {
