9169ad9083d523a33cd38a7d564382ba6d63dec0
Baruch Odem <baruchiro@gmail.com>
2020-10-18 20:42:18 +0300

    remove unused

--- src/originalBudgetTrackingApp/transactions/dates.ts
+++ src/originalBudgetTrackingApp/transactions/dates.ts
@@ -1,5 +1,3 @@
-export const formatDate = (date: Date) => `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
-
 export const compareObjectsByDate = (a: { date: string }, b: { date: string }) => compareDateStrings(a.date, b.date);
 const compareDateStrings = (a: string, b: string) => compareDates(new Date(a), new Date(b));
 const compareDates = (a: Date, b: Date) => a.getTime() - b.getTime();
