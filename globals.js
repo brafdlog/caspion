e703226b18634ffa2696eccc1af25d36061eaeeb
Baruch Odem <baruchiro@gmail.com>
2020-11-05 18:55:17 +0200

    refactor globals to be easy to change

--- globals.js
+++ globals.js
@@ -1,5 +1,6 @@
-module.exports = [
-  'SENTRY_DSN',
-  'GOOGLE_CLIENT_ID',
-  'GOOGLE_CLIENT_SECRET',
-];
+module.exports = {
+  SENTRY_DSN: JSON.stringify(process.env.SENTRY_DSN),
+  // https://console.developers.google.com/apis/credentials
+  GOOGLE_CLIENT_ID: JSON.stringify(process.env.GOOGLE_CLIENT_ID),
+  GOOGLE_CLIENT_SECRET: JSON.stringify(process.env.GOOGLE_CLIENT_SECRET),
+};
