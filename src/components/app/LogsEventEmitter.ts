ec26ba42788a84abffeb6e62fd5fa159b490140d
Jonathan <brafdlog@gmail.com>
2020-12-14 14:08:56 +0200

    Simplify logs event event emitter and make it work with the BudgetTrackingEvent type

--- src/components/app/LogsEventEmitter.ts
+++ src/components/app/LogsEventEmitter.ts
@@ -1,20 +1,13 @@
-import { EventEmitter } from '@/originalBudgetTrackingApp';
-import { EventNames } from '@/originalBudgetTrackingApp/eventEmitters/EventEmitter';
-import { Levels, LogEntry } from '../shared/log/types';
+import { EventEmitter, BudgetTrackingEvent } from '@/originalBudgetTrackingApp';
+import { Levels } from '../shared/log/types';
 
-export default (callback: (entry: LogEntry) => void) => {
+export default (callback: (entry: BudgetTrackingEvent & { level: Levels }) => void) => {
   const eventPublisher = new EventEmitter.BudgetTrackingEventEmitter();
 
   eventPublisher.onAny((eventName, data) => {
     const message = data?.message || eventName;
-
-    switch (eventName) {
-      case EventNames.GENERAL_ERROR:
-      case EventNames.IMPORTER_ERROR:
-        return callback({ message, level: Levels.Error });
-      default:
-        return callback({ message, level: Levels.Info });
-    }
+    const logLevel = data?.error ? Levels.Error : Levels.Info;
+    return callback({ ...data, message, level: logLevel });
   });
 
   return eventPublisher;
