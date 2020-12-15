97e67456bfd107421fa4c45c29ba78185129adec
Jonathan <brafdlog@gmail.com>
2020-12-15 12:58:18 +0200

    Show status indication on account card

--- src/components/shared/log/AccountCard.vue
+++ src/components/shared/log/AccountCard.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="card-container">
     <v-card elevation="4">
-      <v-card-title> {{ accountState.name }}</v-card-title>
-      <v-card-text> {{ latestEventMessage }}</v-card-text>
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title v-text="accountState.name" />
+            <v-list-item-subtitle v-text="latestEventMessage" />
+          </v-list-item-content>
+          <v-list-item-icon class="status-indicator-wrapper">
+            <account-card-status-indicator :account-status="accountState.status" />
+          </v-list-item-icon>
+        </v-list-item>
+      </v-list>
     </v-card>
   </div>
 </template>
@@ -10,8 +19,12 @@
 <script lang="ts">
 import { computed, defineComponent } from '@vue/composition-api';
 import { AccountState } from '@/components/app/accountsState';
+import AccountCardStatusIndicator from '@/components/shared/log/AccountCardStatusIndicator.vue';
 
 export default defineComponent({
+  components: {
+    AccountCardStatusIndicator
+  },
   props: {
     accountState: {
       type: Object,
@@ -24,6 +37,7 @@ export default defineComponent({
       const latestEvent = events.length ? events[events.length - 1] : null;
       return latestEvent ? latestEvent.message : '';
     });
+
     return { latestEventMessage };
   }
 });
@@ -33,5 +47,8 @@ export default defineComponent({
 .card-container {
   margin: 20px;
   max-width: 600px;
+}
+.status-indicator-wrapper {
+  margin-right: 20px;
 }
 </style>
