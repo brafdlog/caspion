4bcdb0d87af44e086efaff58ac73e0c228f38cfb
Baruch Odem <baruchiro@gmail.com>
2020-12-24 23:18:42 +0200

    Example of one promise for all

--- src/backend/import/downloadChromium.js
+++ src/backend/import/downloadChromium.js
@@ -3,10 +3,17 @@ import { getPuppeteerConfig } from 'israeli-bank-scrapers-core';
 
 const revision = getPuppeteerConfig().chromiumRevision;
 
+let pathPrms
+
 export default async function downloadChromium(installPath, onProgress) {
-  return download({
+  console.log({ pathPrms })
+  if (pathPrms) return pathPrms;
+
+  pathPrms = download({
     revision,
     installPath,
     onProgress,
-  });
+  })
+
+  return pathPrms
 }
