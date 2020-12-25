b57ca7941fb650d40fe6b165cf36710ee56f5a21
Baruch Odem <baruchiro@gmail.com>
2020-12-25 12:24:58 +0200

    Revert "Example of one promise for all"
    This reverts commit 4bcdb0d87af44e086efaff58ac73e0c228f38cfb.

--- src/backend/import/downloadChromium.js
+++ src/backend/import/downloadChromium.js
@@ -3,17 +3,10 @@ import { getPuppeteerConfig } from 'israeli-bank-scrapers-core';
 
 const revision = getPuppeteerConfig().chromiumRevision;
 
-let pathPrms
-
 export default async function downloadChromium(installPath, onProgress) {
-  console.log({ pathPrms })
-  if (pathPrms) return pathPrms;
-
-  pathPrms = download({
+  return download({
     revision,
     installPath,
     onProgress,
-  })
-
-  return pathPrms
+  });
 }
