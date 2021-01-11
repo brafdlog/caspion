c6bd8eaa61893c6d03464f7f87f381dc0c151c2b
Baruch Odem <baruchiro@gmail.com>
2021-01-11 20:44:37 +0200

    use local userData folder on development

--- src/backend/consts.ts
+++ src/backend/consts.ts
@@ -1,5 +1,10 @@
 import { app, remote } from 'electron';
+import path from 'path';
 
 const App = app || remote.app;
+
+if (process.env.NODE_ENV !== 'production') {
+  App.setPath('userData', path.resolve('userData'));
+}
 
 export const userDataPath = App.getPath('userData');
