7d710418cc645c5d361827909a01dbc18f8619db
Baruch Odem <baruchiro@gmail.com>
2021-01-16 19:51:12 +0200

    fix UI overflow

--- src/ui/components/app/importers/DeleteImporterDialog.vue
+++ src/ui/components/app/importers/DeleteImporterDialog.vue
@@ -1,41 +1,39 @@
 <template>
-  <v-row justify="center">
-    <v-dialog
-      v-model="show"
-      persistent
-      max-width="650"
-    >
-      <v-card>
-        <v-card-title
-          class="headline"
+  <v-dialog
+    v-model="show"
+    persistent
+    max-width="650"
+  >
+    <v-card>
+      <v-card-title
+        class="headline"
+      >
+        Warning
+        <v-icon color="info">
+          mdi-alert-circle
+        </v-icon>
+      </v-card-title>
+      <v-card-text>
+        This will permanently delete the importer. Continue?
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn
+          text
+          @click="show = false"
         >
-          Warning
-          <v-icon color="info">
-            mdi-alert-circle
-          </v-icon>
-        </v-card-title>
-        <v-card-text>
-          This will permanently delete the importer. Continue?
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            text
-            @click="show = false"
-          >
-            Cancel
-          </v-btn>
-          <v-btn
-            color="primary"
-            text
-            @click="confirm"
-          >
-            OK
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-row>
+          Cancel
+        </v-btn>
+        <v-btn
+          color="primary"
+          text
+          @click="confirm"
+        >
+          OK
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
