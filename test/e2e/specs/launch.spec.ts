4c89e1d2109b44bbdcf8b5be37fc7b5345ce7ca5
Baruch Odem <baruchiro@gmail.com>
2021-03-15 21:07:24 +0200

    try

--- test/e2e/specs/launch.spec.ts
+++ test/e2e/specs/launch.spec.ts
@@ -12,7 +12,7 @@ const screenshotsDir = './screenshots';
 jest.setTimeout(1000000);
 
 // Remove when https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/625 closed
-const skip = process.env.GITHUB_ACTIONS && process.platform === 'win32';
+const skip = false; // process.env.GITHUB_ACTIONS && process.platform === 'win32';
 
 (skip ? describe.skip : describe)('Launch', () => {
   let app: Application;
