2c66c2c1acdc94eec90b134ee14a358012aae3c8
barak <baaraak0@gmail.com>
2021-06-07 23:03:28 +0300

    create setup exporter function

--- src/ui/components/app/exporters/CsvExporter.vue
+++ src/ui/components/app/exporters/CsvExporter.vue
@@ -14,9 +14,8 @@
       label="CSV file"
       outlined
       :rules="rules"
-      append-icon="mdi-folder-open"
-      @click:append="openNativeDialog"
-      @change="changed = true"
+      readonly
+      @click="click()"
     />
     <v-btn
       color="primary"
@@ -30,36 +29,56 @@
 
 <script lang="ts">
 import { ipcRenderer } from 'electron';
-import { setupExporterConfigForm } from '@/ui/components/app/exporters/exportersCommon';
 import { OutputVendorName } from '@/backend/commonTypes';
 import { legalPath, required } from '@/ui/components/shared/formValidations';
-import { defineComponent } from '@vue/composition-api';
+import {
+  defineComponent, computed, reactive, ref
+} from '@vue/composition-api';
+import store from '@/ui/store';
+import { VForm } from '@/types/vuetify';
+
+import { cloneDeep } from 'lodash';
+
+function createSetupConfigForm() {
+  const vForm = ref<VForm>();
+
+  const exporter = reactive(cloneDeep(store.getters.Config.getExporter(OutputVendorName.CSV)));
+  const validated = ref(true);
+  const changed = ref(false);
+  const readyToSave = computed(() => validated.value && changed.value);
+
+  const submit = async () => {
+    if (vForm.value?.validate()) {
+      await store.dispatch.Config.updateExporter({ name: OutputVendorName.CSV, exporter });
+      changed.value = false;
+    }
+  };
+
+  const click = async () => {
+    const filePath = await ipcRenderer.invoke('choose-dir');
+    if (filePath) {
+      exporter.options.filePath = filePath;
+      changed.value = true;
+    }
+  };
+
+  return {
+    validated, changed, readyToSave, submit, exporter, vForm, click
+  };
+}
 
 export default defineComponent({
   name: 'CsvExporter',
 
   setup() {
     return {
-      ...setupExporterConfigForm(OutputVendorName.CSV),
+      ...createSetupConfigForm(),
       rules: [
         required,
         legalPath
       ]
     };
   },
-  data(data) {
-    return {
-      filePath: data.exporter.options.filePath,
-    };
-  },
-  methods: {
-    async openNativeDialog() {
-      const filePath = await ipcRenderer.invoke('choose-dir');
-
-      this.filePath = filePath;
-      this.$emit('change', filePath);
-    }
-  }
 });
 
 </script>
