09215a0b428b3bdaf175a9c7a6a57781145cba64
Jonathan Goldfarb <brafdlog@gmail.com>
2021-10-29 22:46:21 +0300

    Fix tests

--- ui-react/src/setupTests.ts
+++ ui-react/src/setupTests.ts
@@ -3,3 +3,19 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const MOCK_ELECTRON = {
+    ipcRenderer: {
+        invoke: () => {},
+        send: () => {}
+    }
+};
+
+// @ts-ignore
+window.require = (name: string) => {
+    if (name === 'electron') {
+        return MOCK_ELECTRON;
+    } else {
+        throw new Error(`No mock for module ${name}`);
+    }
+};
