3fb83d4ea5c93f9c453298425faabec3cee9d186
michlus <110856936+michlus@users.noreply.github.com>
2022-08-11 05:28:32 +0300

    Update googleSheets.ts
    Following https://github.com/brafdlog/caspion/pull/366:
    1. Splitted the const with field names in several lines as it was too long
    2. Changed the order of the fields. The previously order remained, new fields are added thereafter.
    3. Removed the newly-add field 'installments' since it created an error.

--- src/backend/export/outputVendors/googleSheets/googleSheets.ts
+++ src/backend/export/outputVendors/googleSheets/googleSheets.ts
@@ -11,7 +11,11 @@ import { appendToSpreadsheet } from './googleSheetsInternalAPI';
 
 const GOOGLE_SHEETS_DATE_FORMAT = 'DD/MM/YYYY';
 const DEFAULT_SHEET_NAME = '_caspion';
-const COLUMN_HEADERS = ['תאריך', 'סכום', 'תיאור', 'תיאור נוסף', 'קטגוריה', 'מספר חשבון', 'hash - לא לגעת', 'תאריך חיוב', 'מזהה', 'סטטוס', 'סוג', 'תשלומים', 'סכום מקור', 'מטבע'];
+const COLUMN_HEADERS = [
+'תאריך', 'סכום', 'תיאור', 'תיאור נוסף',
+ 'קטגוריה', 'מספר חשבון', 'hash - לא לגעת',
+ 'מטבע','סכום מקורי', 'ת. חיוב', 'מזהה', 'סוג', 'סטטוס'
+];
 
 const createTransactionsInGoogleSheets: ExportTransactionsFunction = async (
   { transactionsToCreate: transactions, outputVendorsConfig },
@@ -44,13 +48,12 @@ const createTransactionsInGoogleSheets: ExportTransactionsFunction = async (
     transaction.category,
     transaction.accountNumber,
     transaction.hash,
+    transaction.originalCurrency,
+    transaction.originalAmount,
     moment(transaction.processedDate).format(GOOGLE_SHEETS_DATE_FORMAT),
     transaction.identifier,
-    transaction.status,
     transaction.type,
-    transaction.installments,
-    transaction.originalAmount,
-    transaction.originalCurrency
+    transaction.status
   ]);
 
   const spreadsheetAppendResult = await googleSheets.appendToSpreadsheet(
