523d7138e3346c59e7b5fed30388083f4a085772
Baruch Odem <baruchiro@gmail.com>
2022-09-05 21:50:50 +0300

    try semantic-release

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  branches: ['release'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github'
+  ]
+};
