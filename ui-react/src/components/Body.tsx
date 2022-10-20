4d1916393e33a3c068992344d2070e453b2c586d
Jonathan <brafdlog@gmail.com>
2022-10-20 09:04:56 +0300

    Remove modal header name

--- ui-react/src/components/Body.tsx
+++ ui-react/src/components/Body.tsx
@@ -75,7 +75,7 @@ const Body = ({ scrape }: BodyProps) => {
           </Stack>
         </div>
         <Modal show={modalStatus !== ModalStatus.Hidden} onHide={closeModal} dialogClassName={wideModal ? styles.modalWide : ''}>
-          <Modal.Header closeButton>
+          <Modal.Header closeButton className={styles.modalHeader}>
           </Modal.Header>
           <Modal.Body>
             { modalStatus === ModalStatus.Logs && currentAccount && currentAccount.logs.map((log, index) => <div key={index}>{log.message}</div>)}
