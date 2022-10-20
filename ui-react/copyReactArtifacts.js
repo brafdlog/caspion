b0dfa1150e2632109e1722bb460f5b4d06411204
Jonathan <brafdlog@gmail.com>
2022-10-20 09:34:57 +0300

    Copy static folder instead of fixing paths

--- ui-react/copyReactArtifacts.js
+++ ui-react/copyReactArtifacts.js
@@ -1,29 +1,21 @@
 const fse = require('fs-extra');
-const fs = require('fs');
 const path = require('path');
 
 const reactBuildDirPath = path.join(__dirname, 'build');
 const publicDirPath = path.join(__dirname, '..', 'public');
 const distReactPath = path.join(publicDirPath, 'react');
-const sourceIndexHtmlPath = path.join(reactBuildDirPath, 'index.html');
+const distStaticResourcesPath = path.join(publicDirPath, 'static');
 
 async function copyReactArtifacts() {
     console.log(`Copying react artifact from ${reactBuildDirPath} to ${distReactPath}`)
 
-    await fixReferencesToStaticFolder(sourceIndexHtmlPath);
-
     // Cleanup the previous build
     fse.removeSync(distReactPath);
+    fse.removeSync(distStaticResourcesPath);
 
     fse.ensureDirSync(distReactPath);
     fse.copySync(reactBuildDirPath, distReactPath);
-}
-
-async function fixReferencesToStaticFolder(filePath) {
-    console.log('Fixing references to static folder');
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    const result = fileContent.replace(/\/static/g, '/react/static');
-    fs.writeFileSync(filePath, result,'utf8');
+    fse.copySync(path.join(distReactPath, 'static'), distStaticResourcesPath);
 }
 
 copyReactArtifacts();
