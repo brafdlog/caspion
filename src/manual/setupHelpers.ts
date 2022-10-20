0cb2db40bf7a09e3a22bb73935687e060677d1d3
Jonathan <brafdlog@gmail.com>
2022-10-20 09:54:45 +0300

    In ynab data don't fetch financial accounts because it takes a long time

--- src/manual/setupHelpers.ts
+++ src/manual/setupHelpers.ts
@@ -3,7 +3,7 @@ import { configFilePath } from '@/app-globals';
 import { FETCH_YNAB_ACCOUNT_DATA_STATUS, OutputVendorName, YnabAccountDataType } from '@/backend/commonTypes';
 import { getConfig } from '@/backend/configManager/configManager';
 import { getYnabAccountDetails, isAccessTokenValid } from '@/backend/export/outputVendors/ynab/ynab';
-import { getFinancialAccountDetails } from '@/backend/import/importTransactions';
+// import { getFinancialAccountDetails } from '@/backend/import/importTransactions';
 
 export async function getYnabAccountData(): Promise<YnabAccountDataType> {
   const config = await getConfig(configFilePath);
@@ -14,9 +14,12 @@ export async function getYnabAccountData(): Promise<YnabAccountDataType> {
       status: FETCH_YNAB_ACCOUNT_DATA_STATUS.INVALID_ACCESS_TOKEN
     };
   }
-  const ynabAccountDataPromise = getYnabAccountDetails(config.outputVendors);
-  const financialAccountDetailsPromise = getFinancialAccountDetails();
-  const [ynabAccountData, financialAccountDetails] = await Promise.all([ynabAccountDataPromise, financialAccountDetailsPromise]);
+  // const ynabAccountDataPromise = getYnabAccountDetails(config.outputVendors);
+  // const financialAccountDetailsPromise = getFinancialAccountDetails();
+  // const [ynabAccountData, financialAccountDetails] = await Promise.all([ynabAccountDataPromise, financialAccountDetailsPromise]);
+  const ynabAccountData = await getYnabAccountDetails(config.outputVendors);
+  const financialAccountDetails = [];
+
   return {
     status: ynabAccountData.categories ? FETCH_YNAB_ACCOUNT_DATA_STATUS.SUCCESS : FETCH_YNAB_ACCOUNT_DATA_STATUS.INVALID_BUDGET_ID,
     ynabAccountData,
