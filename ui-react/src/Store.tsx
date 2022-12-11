c08f058f5551c7505560a92726ab69cd354cc68a
Baruch Odem <baruchiro@gmail.com>
2022-12-11 12:05:00 +0200

    diable no-console for store

--- ui-react/src/Store.tsx
+++ ui-react/src/Store.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { action, makeAutoObservable } from 'mobx';
 import { createContext } from 'react';
 import { updateConfig, getYnabAccountData } from './eventsBridge';
