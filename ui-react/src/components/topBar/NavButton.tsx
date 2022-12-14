d51279ab552abf823395a0be90d380e2fd09e88c
Erez Birenholz <biran330@gmail.com>
2022-12-14 15:54:53 +0200

    PR code review fixes

--- ui-react/src/components/topBar/NavButton.tsx
+++ ui-react/src/components/topBar/NavButton.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import navEllipseIcon from "../../assets/navEllipse.svg"
+import React from "react";
+import { Button, Stack } from "react-bootstrap";
+import navEllipseIcon from "../../assets/navEllipse.svg";
 
-function NavButton({onClick, text}) {
+function NavButton({ onClick, text }) {
   return (
-    <div>
-     <img src={navEllipseIcon} alt='navEllipse' style={{cursor: "pointer"}}/>
-      <Button variant="light" onClick={onClick} style={{backgroundColor:"transparent", borderWidth:0}}>{text}</Button> 
-    </div>
-  )
+    <Button
+      variant="light"
+      onClick={onClick}
+      style={{ backgroundColor: "transparent", borderWidth: 0 }}
+    >
+      <Stack direction="horizontal">
+        <div
+          style={{
+            cursor: "pointer",
+            backgroundImage: `url(${navEllipseIcon})`,
+            backgroundPosition: 'center',
+            backgroundRepeat  : 'no-repeat',
+            width: 50,
+            height: 50,
+          }}
+        />
+        {text}
+      </Stack>
+    </Button>
+  );
 }
 
 export default NavButton;
