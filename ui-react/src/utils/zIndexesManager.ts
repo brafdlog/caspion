8a9a78e7049adf4a3188875a800f3bcd3142d783
Erez Birenholz <biran330@gmail.com>
2022-12-19 03:20:21 +0200

    fix spelling mistake

--- ui-react/src/utils/zIndexesManager.ts
+++ ui-react/src/utils/zIndexesManager.ts
@@ -1,4 +1,4 @@
-const maxZIndexUsedByReactBootsrap = 10000;
+const maxZIndexUsedByReactBootstrap = 10000;
 
 type ZIndexLayers = "modal" | "offcanvas";
 
@@ -6,7 +6,7 @@ export let zIndexes: Record<ZIndexLayers, number>;
 
 const makeZIndexes = (layers: ZIndexLayers[]) => {
   return layers.reduce((prevValue, layerName, index) => {
-    prevValue[layerName] = (index + 1) * maxZIndexUsedByReactBootsrap;
+    prevValue[layerName] = (index + 1) * maxZIndexUsedByReactBootstrap;
     return prevValue;
   }, {} as Record<ZIndexLayers, number>);
 };
