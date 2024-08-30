6958d1ebd7b009e9563b49807e07d32b8e6138b0
Jonathan <brafdlog@gmail.com>
2024-08-30 22:31:25 +0300

    Fix longstanding mobx warning about changing state outside of action

--- packages/renderer/src/store/Store.tsx
+++ packages/renderer/src/store/Store.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import { getYnabAccountData } from '#preload';
-import { action, makeAutoObservable } from 'mobx';
+import { makeAutoObservable, runInAction } from 'mobx';
 import { createContext, useContext } from 'react';
 import { type YnabAccountDataType, type YnabConfig } from '../types';
 
@@ -13,18 +13,18 @@ export default class Store {
 
   constructor() {
     this.fetchingYnabAccountData = false;
-    // TODO: remove override
-    makeAutoObservable(this, {
-      fetchYnabAccountData: action,
-    });
+    makeAutoObservable(this);
   }
 
   async fetchYnabAccountData(ynabOptions: YnabConfig['options']) {
     console.log('Fetching ynab account data');
     this.fetchingYnabAccountData = true;
-    this.ynabAccountData = await getYnabAccountData(ynabOptions);
-    this.fetchingYnabAccountData = false;
-    console.log('Ynab account data ', this.ynabAccountData);
+    const ynabAccountData = await getYnabAccountData(ynabOptions);
+    runInAction(() => {
+      this.ynabAccountData = ynabAccountData;
+      this.fetchingYnabAccountData = false;
+      console.log('Ynab account data ', this.ynabAccountData);
+    });
   }
 
 }
