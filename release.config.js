1dc32e59f869a9b1812eead1b7520f1c395af58f
Baruch Odem <baruchiro@gmail.com>
2024-09-06 13:27:11 +0300

    Add PR Title workflow and release configuration

---
+++ release.config.js
@@ -0,0 +1,18 @@
+const releaseRules = {
+  preset: 'eslint',
+  releaseRules: [
+    { tag: 'Breaking', release: 'major' },
+    { tag: 'Build', release: 'minor' },
+    { tag: 'Chore', release: 'minor' },
+    { tag: 'Fix', release: 'patch' },
+    { tag: 'New', release: 'minor' },
+    { tag: 'Update', release: 'minor' },
+    { tag: 'Upgrade', release: 'minor' },
+    { tag: 'Deps', release: 'patch' },
+  ],
+};
+
+export default {
+  branches: ['master'],
+  plugins: [['@semantic-release/commit-analyzer', releaseRules], '@semantic-release/github'],
+};
