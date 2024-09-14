e4142663ec24a43c807aefdcf73cbd1c3d8cea4c
shai.ungar <shai.ungar@riskified.com>
2024-09-15 01:31:36 +0300

    Fix YNAB budget when there is only one budget id

--- ui-react/src/components/exporters/EditYnabExporter.tsx
+++ ui-react/src/components/exporters/EditYnabExporter.tsx
@@ -2,9 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { toJS } from 'mobx';
 import Spinner from 'react-bootstrap/Spinner';
 import { observer } from 'mobx-react-lite';
-import {
-  Button, Card, Form, Image
-} from 'react-bootstrap';
+import { Button, Card, Form, Image } from 'react-bootstrap';
 import styles from './EditYnabExporter.module.css';
 import { YnabConfig } from '../../types';
 
@@ -14,36 +12,52 @@ import { StoreContext } from '../../Store';
 type EditYnabExporterProps = {
   handleSave: (exporterConfig: YnabConfig) => Promise<void>;
   exporterConfig: YnabConfig;
-}
+};
 
 const INVALID_ACCESS_TOKEN = 'INVALID_ACCESS_TOKEN';
 
-const EditYnabExporter = ({ handleSave, exporterConfig }: EditYnabExporterProps) => {
-  const [ynabOptions, setYnabOptions] = useState<YnabConfig['options']>(toJS<YnabConfig['options']>(exporterConfig.options));
+const EditYnabExporter = ({
+  handleSave,
+  exporterConfig,
+}: EditYnabExporterProps) => {
+  const [ynabOptions, setYnabOptions] = useState<YnabConfig['options']>(
+    toJS<YnabConfig['options']>(exporterConfig.options),
+  );
   const [active, setActive] = useState<boolean>(exporterConfig.active);
   const store = useContext(StoreContext);
 
   const isLoading = !store.ynabAccountData || store.fetchingYnabAccountData;
-  const isValidAccessToken = !isLoading && store.ynabAccountData?.status !== INVALID_ACCESS_TOKEN;
+  const isValidAccessToken =
+    !isLoading && store.ynabAccountData?.status !== INVALID_ACCESS_TOKEN;
 
   useEffect(() => {
     if (ynabOptions.accessToken) {
       store.fetchYnabAccountData(ynabOptions);
     }
+    if (store.ynabAccountData?.ynabAccountData?.budgets.length === 1) {
+      const singleBudgetId =
+        store.ynabAccountData.ynabAccountData.budgets[0].id;
+      setYnabOptions((prevOptions) => ({
+        ...prevOptions,
+        budgetId: singleBudgetId,
+      }));
+    }
     // eslint-disable-next-line
   }, [ynabOptions.budgetId, ynabOptions.accessToken, store]);
 
-  const updateOptionsState = (optionUpdates: Partial<YnabConfig['options']>) => {
+  const updateOptionsState = (
+    optionUpdates: Partial<YnabConfig['options']>,
+  ) => {
     setYnabOptions({
       ...ynabOptions,
-      ...optionUpdates
+      ...optionUpdates,
     });
   };
   const handleSaveClick = async () => {
     await handleSave({
       ...exporterConfig,
       active,
-      options: ynabOptions
+      options: ynabOptions,
     });
   };
 
@@ -51,53 +65,94 @@ const EditYnabExporter = ({ handleSave, exporterConfig }: EditYnabExporterProps)
     updateOptionsState({ [propertyName]: event.target.value });
   };
 
-  const handleAccountMappingChange = (updatedAccountMapping: YnabConfig['options']['accountNumbersToYnabAccountIds']) => {
-    updateOptionsState({ accountNumbersToYnabAccountIds: updatedAccountMapping });
+  const handleAccountMappingChange = (
+    updatedAccountMapping: YnabConfig['options']['accountNumbersToYnabAccountIds'],
+  ) => {
+    updateOptionsState({
+      accountNumbersToYnabAccountIds: updatedAccountMapping,
+    });
   };
 
   return (
     <div className={styles.container}>
       <Card className={styles.card}>
-        <Image className={styles.logo} src={exporterConfig.logo} roundedCircle width={100} height={100} />
+        <Image
+          className={styles.logo}
+          src={exporterConfig.logo}
+          roundedCircle
+          width={100}
+          height={100}
+        />
         <Card.Body className={styles.cardBody}>
           <Form className={styles.form}>
-            <Form.Group controlId='accessToken' className='mb-3'>
+            <Form.Group controlId="accessToken" className="mb-3">
               <Form.Label>Ynab access token</Form.Label>
-              <Form.Control type='password' value={ynabOptions.accessToken} onChange={(event) => handleOptionChangeEvent('accessToken', event)} />
+              <Form.Control
+                type="password"
+                value={ynabOptions.accessToken}
+                onChange={(event) =>
+                  handleOptionChangeEvent('accessToken', event)
+                }
+              />
             </Form.Group>
-            {isValidAccessToken
-              && <Form.Group controlId='budgetId' className='mb-3'>
+            {isValidAccessToken && (
+              <Form.Group controlId="budgetId" className="mb-3">
                 <Form.Label>Budget id</Form.Label>
                 <Form.Select
                   disabled={store.fetchingYnabAccountData}
                   defaultValue={ynabOptions.budgetId}
-                  onChange={(event) => handleOptionChangeEvent('budgetId', event)}>
-                  {store.ynabAccountData?.ynabAccountData?.budgets.map((budget) => <option key={budget.id} value={budget.id}>{budget.name}</option>)}
+                  onChange={(event) =>
+                    handleOptionChangeEvent('budgetId', event)
+                  }
+                >
+                  {store.ynabAccountData?.ynabAccountData?.budgets.map(
+                    (budget) => (
+                      <option key={budget.id} value={budget.id}>
+                        {budget.name}
+                      </option>
+                    ),
+                  )}
                 </Form.Select>
               </Form.Group>
-            }
-            {!isLoading && !isValidAccessToken && <div>Invalid access token</div>}
-            {isLoading && <Spinner style={{ width: '20px', height: '20px' }} animation="border" variant="primary" />}
-            {!isLoading && isValidAccessToken
-              && <Form.Group controlId='accountNumbersToYnabAccountIds' className='mb-3'>
+            )}
+            {!isLoading && !isValidAccessToken && (
+              <div>Invalid access token</div>
+            )}
+            {isLoading && (
+              <Spinner
+                style={{ width: '20px', height: '20px' }}
+                animation="border"
+                variant="primary"
+              />
+            )}
+            {!isLoading && isValidAccessToken && (
+              <Form.Group
+                controlId="accountNumbersToYnabAccountIds"
+                className="mb-3"
+              >
                 <YnabAccountMappingTable
-                  accountNumberToYnabIdMapping={ynabOptions.accountNumbersToYnabAccountIds}
+                  accountNumberToYnabIdMapping={
+                    ynabOptions.accountNumbersToYnabAccountIds
+                  }
                   onUpdate={handleAccountMappingChange}
                   ynabAccountData={store.ynabAccountData}
-                  budgetId={ynabOptions.budgetId} />
+                  budgetId={ynabOptions.budgetId}
+                />
               </Form.Group>
-            }
-            <Form.Group controlId='exporterActive'>
+            )}
+            <Form.Group controlId="exporterActive">
               <Form.Check
-                type='switch'
+                type="switch"
                 onChange={() => setActive(!active)}
-                label='Active'
+                label="Active"
                 checked={active}
               />
             </Form.Group>
           </Form>
           <div className={styles.actionButtonsWrapper}>
-            <Button variant='primary' onClick={handleSaveClick}>שמור</Button>
+            <Button variant="primary" onClick={handleSaveClick}>
+              שמור
+            </Button>
           </div>
         </Card.Body>
       </Card>
