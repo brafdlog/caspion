016900fd8c4ca569185260eddbe2157f31a5303b
shai.ungar <shai.ungar@riskified.com>
2024-09-20 10:44:11 +0300

    logs

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -18,11 +18,8 @@ afterAll(async () => {
 });
 
 test('Main window state', async () => {
-  console.log('start');
   const page = await electronApp.firstWindow();
-  console.log('await electronApp.firstWindow');
   const window: JSHandle<BrowserWindow> = await electronApp.browserWindow(page);
-  console.log('await electronApp.browserWindow(page)');
   const windowState = await window.evaluate(
     (
       mainWindow,
@@ -38,18 +35,12 @@ test('Main window state', async () => {
       });
 
       return new Promise((resolve) => {
-        /**
-         * The main window is created hidden, and is shown only when it is ready.
-         * See {@link ../packages/main/src/mainWindow.ts} function
-         */
         if (mainWindow.isVisible()) {
           resolve(getState());
         } else mainWindow.once('ready-to-show', () => resolve(getState()));
       });
     },
   );
-
-  console.log('await window.evaluate');
 
   expect(windowState.isCrashed, 'The app has crashed').toBeFalsy();
   expect(windowState.isVisible, 'The main window was not visible').toBeTruthy();
@@ -61,6 +52,10 @@ test('Main window state', async () => {
 
 test('Main window web content', async () => {
   const page = await electronApp.firstWindow();
+
+  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+  page.on('pageerror', err => console.log('PAGE ERROR:', err));
+
   const element = await page.$('#app', { strict: true });
   expect(element, 'Was unable to find the root element').toBeDefined();
   expect(
@@ -77,9 +72,6 @@ test('Preload versions', async () => {
     'expect find one element #process-versions',
   ).toStrictEqual(1);
 
-  /**
-   * In this test we check only text value and don't care about formatting. That's why here we remove any space symbols
-   */
   const renderedVersions = (await versionsElement.innerText()).replace(
     /\s/g,
     '',
@@ -96,7 +88,6 @@ test('Preload versions', async () => {
 test('Preload nodeCrypto', async () => {
   const page = await electronApp.firstWindow();
 
-  // Test hashing a random string
   const testString = Math.random().toString(36).slice(2, 7);
 
   const rawInput = page.locator('input#reactive-hash-raw-value');
