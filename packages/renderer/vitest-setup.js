df360d0f64c63939b114a95d7b821aa156a48c24
shai.ungar <shai.ungar@riskified.com>
2024-09-21 19:27:59 +0300

    fix

--- packages/renderer/vitest-setup.js
+++ packages/renderer/vitest-setup.js
@@ -1,16 +1,40 @@
 import { vi } from 'vitest';
 
-vi.mock('electron', () => ({
-  default: {
-    ipcRenderer: {
-      invoke: vi.fn(),
-      send: vi.fn(),
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false, // You can set this to true if you want to simulate a matching media query
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // Deprecated method
+    removeListener: vi.fn(), // Deprecated method
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
+
+vi.mock('electron', () => {
+  const invokeSpy = vi.fn((channel) => {
+    if (channel === 'getAppInfo') {
+      return Promise.resolve({ name: 'Test App', version: '1.0.0' });
+    }
+    if (channel === 'getConfig') {
+      return Promise.resolve(JSON.stringify({ config: { someKey: 'someValue' } }));
+    }
+    if (channel === 'getLogsInfo') {
+      return Promise.resolve({ logsFolder: '/path/to/logs', otherData: 'someValue' });  // Ensure logsFolder is defined
+    }
+    return Promise.resolve(null);
+  });
+  return {
+    default: {
+      ipcRenderer: {
+        invoke: invokeSpy,
+      },
     },
-    shell: {
-      openExternal: vi.fn(),
-    },
-  },
-}));
+  };
+});
 
 global.window.require = vi.fn().mockImplementation((module) => {
   if (module === 'electron') {
