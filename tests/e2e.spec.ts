58c2f9b608acaef87fae5ad1917d204e624241b1
shai.ungar <shai.ungar@riskified.com>
2024-09-21 21:57:01 +0300

    remove tests

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -18,11 +18,8 @@ afterAll(async () => {
 });
 
 test('Main window state', async () => {
-  console.log('start');
   const page = await electronApp.firstWindow();
-  console.log('await electronApp.firstWindow');
   const window: JSHandle<BrowserWindow> = await electronApp.browserWindow(page);
-  console.log('await electronApp.browserWindow(page)');
   const windowState = await window.evaluate(
     (
       mainWindow,
@@ -49,8 +46,6 @@ test('Main window state', async () => {
     },
   );
 
-  console.log('await window.evaluate');
-
   expect(windowState.isCrashed, 'The app has crashed').toBeFalsy();
   expect(windowState.isVisible, 'The main window was not visible').toBeTruthy();
   expect(
@@ -67,52 +62,4 @@ test('Main window web content', async () => {
     (await element.innerHTML()).trim(),
     'Window content was empty',
   ).not.equal('');
-});
-
-test('Preload versions', async () => {
-  const page = await electronApp.firstWindow();
-  const versionsElement = page.locator('#process-versions');
-  expect(
-    await versionsElement.count(),
-    'expect find one element #process-versions',
-  ).toStrictEqual(1);
-
-  /**
-   * In this test we check only text value and don't care about formatting. That's why here we remove any space symbols
-   */
-  const renderedVersions = (await versionsElement.innerText()).replace(
-    /\s/g,
-    '',
-  );
-  const expectedVersions = await electronApp.evaluate(() => process.versions);
-
-  for (const expectedVersionsKey in expectedVersions) {
-    expect(renderedVersions).include(
-      `${expectedVersionsKey}:v${expectedVersions[expectedVersionsKey]}`,
-    );
-  }
-});
-
-test('Preload nodeCrypto', async () => {
-  const page = await electronApp.firstWindow();
-
-  // Test hashing a random string
-  const testString = Math.random().toString(36).slice(2, 7);
-
-  const rawInput = page.locator('input#reactive-hash-raw-value');
-  expect(
-    await rawInput.count(),
-    'expect find one element input#reactive-hash-raw-value',
-  ).toStrictEqual(1);
-
-  const hashedInput = page.locator('input#reactive-hash-hashed-value');
-  expect(
-    await hashedInput.count(),
-    'expect find one element input#reactive-hash-hashed-value',
-  ).toStrictEqual(1);
-
-  await rawInput.fill(testString);
-  const renderedHash = await hashedInput.inputValue();
-  const expectedHash = createHash('sha256').update(testString).digest('hex');
-  expect(renderedHash).toEqual(expectedHash);
 });
