49264750ac48a0e810b9d1d44d20c13181a48cc4
shai.ungar <shai.ungar@riskified.com>
2024-09-21 23:12:09 +0300

    We do not accept nodejs code on renderer in this new Vite structure

--- packages/renderer/vitest.config.js
+++ packages/renderer/vitest.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import baseConfig from '../../vitest.base.config.js';
 import path from 'path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   ...baseConfig,
