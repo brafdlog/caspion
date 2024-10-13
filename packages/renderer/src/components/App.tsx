c8c8c4d3ac860ffd9930406205f2354b87a7e4d9
Baruch Odem <baruchiro@gmail.com>
2024-10-13 17:38:14 +0300

    remove what blocks the test

--- packages/renderer/src/components/App.tsx
+++ packages/renderer/src/components/App.tsx
@@ -1,14 +1,9 @@
-import { useEffect } from 'react';
 import { StoresProvider } from '../store';
 import './App.css';
 import Body from './Body';
 import TopBar from './topBar/TopBar';
-import logger from '../logging/logger';
 
 function App() {
-  useEffect(() => {
-    logger.info(`${App.name} rendered`);
-  }, []);
   return (
     <StoresProvider>
       <div className="App">
