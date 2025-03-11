46cdf9799b41e8cde57a0601527ce1ac669a446d
whatuserever <83753332+whatuserever@users.noreply.github.com>
2025-03-11 23:18:00 +0200

    Fix typo

--- packages/main/src/backend/configManager/configMigration/versions/v1.ts
+++ packages/main/src/backend/configManager/configMigration/versions/v1.ts
@@ -1,9 +1,9 @@
 // V1 adds the version field to the config object
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function migrateV0ToV1(v1Config: any): any {
+export function migrateV0ToV1(v0Config: any): any {
   return {
-    ...v1Config,
+    ...v0Config,
     version: 1,
   };
 }
